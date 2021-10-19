acf15fc6c574fc883cb501c020f1c86f4a9cc547
Doskarin <doskarinmarlen@gmail.com>
2021-10-19 20:06:40 +0300

    max points update

--- Selected algorithm problems/Hard/Geometry/Max points on line.py
+++ Selected algorithm problems/Hard/Geometry/Max points on line.py
@@ -1,3 +1,22 @@
+'''
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: 3
+
+Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+Output: 4
+
+Constraints:
+
+1 <= points.length <= 300
+points[i].length == 2
+-10^4 <= xi, yi <= 10^4
+All the points are unique.
+
+'''
+
+
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         from decimal import Decimal
